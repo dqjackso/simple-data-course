7bd8fc53f15e10b989358fac2682cac13f0946c6
dqjackso <31450465+dqjackso@users.noreply.github.com>
2020-09-02 15:58:26 -0700

    Create sample.rkt

---
+++ sample.rkt
@@ -0,0 +1,12 @@
+#lang racket
+(require 2htdp/image)
+(require test-engine/racket-tests)
+
+; Design a function that consumes two images and produces true if the first is larger than the second
+
+; produce true if the first img is larger than the second img
+
+(check-expect (larger? (rectangle 3 3 "solid" "red") (rectangle 2 2 "solid" "blue")) true)
+(check-expect (larger? (rectangle 3 3 "solid" "red") (rectangle 4 4 "solid" "blue")) false)
+(check-expect (larger? (rectangle 3 3 "solid" "red") (rectangle 3 4 "solid" "blue")) false)
+(check-expect (larger? (rectangle 3 3 "solid" "red") (rectangle 3 2 "solid" "blue")) false)
