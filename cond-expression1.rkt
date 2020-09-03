c7c8815c95bd56e4bb040108e9f43b618afae5db
dqjackso <31450465+dqjackso@users.noreply.github.com>
2020-09-02 20:36:18 -0700

    Create cond-expression1.rkt

---
+++ cond-expression1.rkt
@@ -0,0 +1,16 @@
+(require 2htdp/image)
+
+(define I1 (rectangle 10 20 "solid" "red"))
+(define I2 (rectangle 20 20 "solid" "red"))
+(define I3 (rectangle 20 10 "solid" "red"))
+
+;; Image -> String
+;; produce shape of image, one of "tall", "square" or "wide"
+(check-expect (aspect-ratio I1) "tall")
+(check-expect (aspect-ratio I2) "square")
+(check-expect (aspect-ratio I3) "wide")
+
+(define (aspect-ratio img)
+    (cond [(> (image-height img) (image-width img)) "tall"]
+          [(= (image-height img) (image-width img)) "square"]
+          [(< (image-height img) (image-width img)) "wide"]))
